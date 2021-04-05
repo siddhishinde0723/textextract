a9c889fb7ff16fc383a9c1e5ec76b55a7de371be
Siddhi Shinde <siddhishinde0723@gmail.com>
2021-04-05 21:51:18 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/version.properties

--- app/src/main/java/com/example/textread/Extraction.java
+++ app/src/main/java/com/example/textread/Extraction.java
@@ -1,23 +1,33 @@
 package com.example.textread;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -27,6 +37,9 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -34,12 +47,16 @@ import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
-import javax.annotation.Nullable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Extraction extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -56,6 +73,9 @@ public class Extraction extends AppCompatActivity implements NavigationView.OnNa
     static final int CAPTURE_IMAGE_REQUEST = 1,SELECT_FILE=0;
     private Uri picUri;
     FirebaseUser user;
+    private static final int CAMERA_REQUEST_CODE=200,STORAGE_REQUEST_CODE=400,IMAGE_PICK_GALLERY_CODE=1000,IMAGE_PICK_CAMERA_CODE=1001;
+    String cameraPermission[],storagePermission[];
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,9 +90,18 @@ public class Extraction extends AppCompatActivity implements NavigationView.OnNa
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+        cameraPermission=new String[]{Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        storagePermission=new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_menu);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActivityCompat.requestPermissions(Extraction.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        ActivityCompat.requestPermissions(Extraction.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
 
         sharedPreferences = getSharedPreferences("googleLogin", Context.MODE_PRIVATE);
         getLoginStatus = sharedPreferences.getBoolean("googleLogin", false);
@@ -85,6 +114,11 @@ public class Extraction extends AppCompatActivity implements NavigationView.OnNa
 
         if (getLoginStatus || isGetLoginStatus) {
             navigationView.getMenu().removeItem(R.id.changePass);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    101);
         }
 
         add=findViewById(R.id.add);
@@ -109,14 +143,20 @@ public class Extraction extends AppCompatActivity implements NavigationView.OnNa
                     finish();
                 }
                 else if(items[i].equals("Camera")) {
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent, CAPTURE_IMAGE_REQUEST);
+                    if (!checkCameraPermission()) {
+                        requestCameraPermission();
+                    } else {
+                        pickCamera();
+                    }
 
             }else if(items[i].equals("Gallery")){
 
-                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    intent.setType("image/*");
-                    startActivityForResult(intent.createChooser(intent,"Select File"), SELECT_FILE);
+                    if(!checkStoragePermission()){
+                        requestStoragePermission();
+                    }
+                    else{
+                        pickGallery();
+                    }
 
                 }else if(items[i].equals("Cancel")){
                     dialog.dismiss();
@@ -126,126 +166,135 @@ public class Extraction extends AppCompatActivity implements NavigationView.OnNa
         builder.show();
 
     }
+    private void pickGallery() {
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_GALLERY_CODE);
+    }
 
+    private void pickCamera() {
+        ContentValues values=new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"NewPic");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Image");
+
+        image_uri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(cameraIntent,IMAGE_PICK_CAMERA_CODE);
+
+
+
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this,storagePermission,STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result= ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result= ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1=ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+
+            case CAMERA_REQUEST_CODE:
+
+                if(grantResults.length>0){
+                    boolean cameraAccepted = grantResults[0]==PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[0]==PackageManager.PERMISSION_GRANTED;
+
+                    if(cameraAccepted && writeStorageAccepted){
+                        pickCamera();
+                    }
+                    else{
+                        Toast.makeText(this,"permission denied",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+
+            case STORAGE_REQUEST_CODE:
+
+                if(grantResults.length>0){
+                    boolean writeStorageAccepted = grantResults[0]==PackageManager.PERMISSION_GRANTED;
+
+                    if(writeStorageAccepted){
+                        pickGallery();
+                    }
+                    else{
+                        Toast.makeText(this,"permission denied",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
 
-        if(resultCode == Activity.RESULT_OK){
-            if(requestCode==CAPTURE_IMAGE_REQUEST){
-// CropImage.activity(picUri).setGuidelines(CropImageView.Guidelines.ON).start(this);
-                picUri = data.getData();
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                CropImage.activity(data.getData()).setGuidelines(CropImageView.Guidelines.ON).start(this);
+            }
+            if (requestCode == IMAGE_PICK_CAMERA_CODE) {
+                CropImage.activity(image_uri).setGuidelines(CropImageView.Guidelines.ON).start(this);
+            }
 
-                Bundle bundle = data.getExtras();
-//from bundle, extract the image
-                Bitmap bitmap = (Bitmap) bundle.get("data");
-//set image in imageview
-                gallery.setImageBitmap(bitmap);
-                FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-//2. Get an instance of FirebaseVision
-                FirebaseVision firebaseVision = FirebaseVision.getInstance();
-//3. Create an instance of FirebaseVisionTextRecognizer
-                FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = firebaseVision.getOnDeviceTextRecognizer();
-//4. Create a task to process the image
-                Task<FirebaseVisionText> task = firebaseVisionTextRecognizer.processImage(firebaseVisionImage);
-//5. if task is success
-                task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-                    @Override
-                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
 
-                        String s = firebaseVisionText.getText();
-                        saveToGallery();
-                        Intent intent = new Intent(Extraction.this, Display.class);
-                        intent.putExtra("", s);
-                        startActivity(intent);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                gallery.setImageURI(resultUri);
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) gallery.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
+                if (!recognizer.isOperational()) {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                } else {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> items = recognizer.detect(frame);
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int i = 0; i < items.size(); i++) {
+                        TextBlock myItems = items.valueAt(i);
+                        sb.append(myItems.getValue());
+                        sb.append("\n");
 
                     }
-                });
 
-//6. if task is failure
-                task.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
+                    textView.setText(sb.toString());
+                    String s = textView.getText().toString();
+                    Intent intent = new Intent(Extraction.this, Display.class);
+                    intent.putExtra("", s);
+                    startActivity(intent);
 
-
-            }if(requestCode==SELECT_FILE) {
-// CropImage.activity(data.getData()).setGuidelines(CropImageView.Guidelines.ON).start(this);
-                Uri selectimg = data.getData();
-                gallery.setImageURI(selectimg);
-
-                try {
-                    FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromFilePath(this,selectimg);
-                    FirebaseVision firebaseVision = FirebaseVision.getInstance();
-                    FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = firebaseVision.getOnDeviceTextRecognizer();
-                    Task<FirebaseVisionText> task = firebaseVisionTextRecognizer.processImage(firebaseVisionImage);
-                    task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-                        @Override
-                        public void onSuccess(FirebaseVisionText firebaseVisionText) {
-
-                            String s = firebaseVisionText.getText();
-                            Intent intent = new Intent(Extraction.this, Display.class);
-                            intent.putExtra("", s);
-                            startActivity(intent);
-
-
-                        }
-                    });
-
-                    task.addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(this, "" + error, Toast.LENGTH_SHORT).show();
+
 
             }
         }
-
-
-
-
-
-
     }
-
-    private void saveToGallery() {
-        BitmapDrawable bitmapDrawable = (BitmapDrawable) gallery.getDrawable();
-        Bitmap bitmap = bitmapDrawable.getBitmap();
-
-        FileOutputStream outputStream = null;
-        File file = Environment.getExternalStorageDirectory();
-        File dir = new File(file.getAbsolutePath() + "/Data");
-        dir.mkdirs();
-
-        String filename = String.format("%d.jpg",System.currentTimeMillis());
-        File outFile = new File(dir,filename);
-        try{
-            outputStream = new FileOutputStream(outFile);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
-        try{
-            outputStream.flush();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        try{
-            outputStream.close();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
 
 
     public void ClickMenu(View view) {
